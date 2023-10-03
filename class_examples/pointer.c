418a66c0f19d186f1d1de1dbbc38b079dccdcdd1
LipeAKugel <felipearaujodff@gmail.com>
2023-10-03 13:06:26 -0300

    Pointers and memory allocation examples

---
+++ class_examples/pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int numero, *ptr;
+    numero = 79417;
+    ptr = &numero;
+    *ptr = 90560;
+
+    printf("Address: %p\n", ptr);
+    printf("Number: %d\n", numero);
+
+    return 0;
+}
